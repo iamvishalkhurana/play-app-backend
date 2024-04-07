93756f4bbb4e1ebb5bde255f2bc0aa2ffe41a421
Vishal Khurana <98726988+iamvishalkhurana@users.noreply.github.com>
2024-04-08 00:06:42 +0530

    registration verification

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -103,7 +103,7 @@ const registerUser = asyncHandler(async (req, res) => {
     email: email,
     subject: "Verification email",
     content: `<p>Hii ${fullName}.<br />
-      Please click <a href="http://localhost:5000/api/v1/auth/verify-mail?id=${user._id}">here</a> to verify your email.
+      Please click <a href="https://play-app-backend-eaag.onrender.com/api/v1/auth/verify-mail?id=${user._id}">here</a> to verify your email.
     </p>`,
   };
   const message = await sendMail(mailData);
